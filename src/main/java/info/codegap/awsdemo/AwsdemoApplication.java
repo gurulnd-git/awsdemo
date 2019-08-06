0f1c21e3860dd23e22aef7c85026fa8f140f6ea4
Guru <guru@gmail.com>
2019-08-06 23:44:58 +0530

    init commit

---
+++ src/main/java/info/codegap/awsdemo/AwsdemoApplication.java
@@ -0,0 +1,13 @@
+package info.codegap.awsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsdemoApplication.class, args);
+	}
+
+}
